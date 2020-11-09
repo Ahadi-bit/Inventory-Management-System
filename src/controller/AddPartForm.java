680d0ca5c9ab25b40104633f80e5097fa8319e1d
Jonathan <Jonathan.Payares@centurylink.com>
2020-11-09 13:47:11 -0500

    event for dynamic label change

--- src/controller/AddPartForm.java
+++ src/controller/AddPartForm.java
@@ -4,25 +4,37 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import model.Outsourced;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
 
-public class AddPartForm {
+public class AddPartForm implements Initializable {
 
     Stage stage;
     Parent scene;
+    Random rand = new Random();
 
     @FXML
     private RadioButton inHousebtn;
 
     @FXML
+    private ToggleGroup partType;
+
+    @FXML
     private RadioButton outSourcedbtn;
+
+    @FXML
+    private TextField IdAutoGenTXT;
 
     @FXML
     private TextField addPartNametxt;
@@ -43,6 +55,30 @@ public class AddPartForm {
     private TextField addPartMintxt;
 
     @FXML
+    private Label dynamiclbl;
+
+    @FXML
+    void OnClickInHouse(MouseEvent event) {
+        this.dynamiclbl.setText("Machine ID");
+        System.out.println("in house btn");
+    }
+
+    @FXML
+    void OnClickOutSource(MouseEvent event) {
+        this.dynamiclbl.setText("Company Name");
+        System.out.println("Outsourced");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        partType = new ToggleGroup();
+        inHousebtn.setToggleGroup(partType);
+        outSourcedbtn.setToggleGroup(partType);
+    }
+
+
+
+    @FXML
     void OnActionCancel(ActionEvent event) throws IOException {
         stage = (Stage)((Button)event.getSource()).getScene().getWindow();
         scene = FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"));
@@ -50,8 +86,18 @@ public class AddPartForm {
         stage.show();
     }
 
+
+    public int assignPartNumber(){
+        int randPartID = rand.nextInt();
+        if (randPartID > 1 || randPartID < 999999){
+            randPartID = rand.nextInt();
+        }
+        return randPartID;
+    }
+
     @FXML
     void OnActionSave(ActionEvent event) {
+
 
     }
 }
